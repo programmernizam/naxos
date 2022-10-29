0793cdbf4bf23d8bcbefca903827492219e0f805
programmerNizam <mdnizam12109@gmail.com>
2022-10-29 16:06:06 +0600

    Update features

--- src/Components/Home/Features.js
+++ src/Components/Home/Features.js
@@ -1,13 +1,58 @@
 import React from "react";
-import {
-  BsAlarm,
-  BsChatLeftText,
-  BsFileEarmark,
-  BsShare
-} from "react-icons/bs";
-import { FiLock, FiSettings } from "react-icons/fi";
 import mobile from "../../assets/feature.png";
+import clock from "../../assets/icons/alarm-clock.png";
+import comment from "../../assets/icons/comment.png";
+import file from "../../assets/icons/file.png";
+import lock from "../../assets/icons/padlock.png";
+import setting from "../../assets/icons/settings.png";
+import share from "../../assets/icons/share.png";
 const Features = () => {
+  const features = [
+    {
+      id: 1,
+      name: "Custom Shortcuts",
+      description:
+        "Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.",
+      img: setting,
+    },
+    {
+      id: 2,
+      name: "Secure Integration",
+      description:
+        "Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.",
+      img: lock,
+    },
+    {
+      id: 3,
+      name: "Free Live Chat",
+      description:
+        "Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.",
+      img: comment,
+    },
+  ];
+  const features2 = [
+    {
+      id: 1,
+      name: "Social Share",
+      description:
+        "Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.",
+      img: share,
+    },
+    {
+      id: 2,
+      name: "Merge Files",
+      description:
+        "Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.",
+      img: file,
+    },
+    {
+      id: 3,
+      name: "Action Reminder",
+      description:
+        "Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.",
+      img: clock,
+    },
+  ];
   return (
     <section>
       <div className="container mx-auto py-10">
@@ -21,83 +66,33 @@ const Features = () => {
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-10 my-10 items-center justify-items-center">
           <div className="grid grid-cols-1 gap-5 lg:w-5/6 p-5">
-            <div className="flex flex-row gap-5">
-              <span>
-                <FiSettings className="text-5xl text-secondary" />
-              </span>
-              <div>
-                <h3 className="text-2xl font-bold">Custom Shortcuts</h3>
-                <p className="my-5 text-gray-500">
-                  Semper a augue suscipit, luctus neque purus ipsum neque dolor
-                  primis libero tempus velna culpa expedita.
-                </p>
+            {features.map((feature) => (
+              <div key={feature.id} className="flex flex-row gap-5">
+                <span>
+                  <img src={feature.img} alt="" className="w-24" />
+                </span>
+                <div>
+                  <h3 className="text-2xl font-bold">{feature.name}</h3>
+                  <p className="my-5 text-gray-500">{feature.description}</p>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-row gap-5">
-              <span>
-                <FiLock className="text-5xl text-secondary" />
-              </span>
-              <div>
-                <h3 className="text-2xl font-bold">Secure Integration</h3>
-                <p className="my-5 text-gray-500">
-                  Semper a augue suscipit, luctus neque purus ipsum neque dolor
-                  primis libero tempus velna culpa expedita.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-row gap-5">
-              <span>
-                <BsChatLeftText className="text-5xl text-secondary" />
-              </span>
-              <div>
-                <h3 className="text-2xl font-bold">Free Live Chat</h3>
-                <p className="my-5 text-gray-500">
-                  Semper a augue suscipit, luctus neque purus ipsum neque dolor
-                  primis libero tempus velna culpa expedita.
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
           <div>
             <img src={mobile} alt="" className="max-w-sm" />
           </div>
           <div className="grid grid-cols-1 gap-5 lg:w-5/6 p-5">
-            <div className="flex flex-row gap-5">
-              <span>
-                <BsShare className="text-5xl text-secondary" />
-              </span>
-              <div>
-                <h3 className="text-2xl font-bold">Social Share</h3>
-                <p className="my-5 text-gray-500">
-                  Semper a augue suscipit, luctus neque purus ipsum neque dolor
-                  primis libero tempus velna culpa expedita.
-                </p>
+            {features2.map((feature) => (
+              <div key={feature.id} className="flex flex-row gap-5">
+                <span>
+                  <img src={feature.img} alt="" className="w-24" />
+                </span>
+                <div>
+                  <h3 className="text-2xl font-bold">{feature.name}</h3>
+                  <p className="my-5 text-gray-500">{feature.description}</p>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-row gap-5">
-              <span>
-                <BsFileEarmark className="text-5xl text-secondary" />
-              </span>
-              <div>
-                <h3 className="text-2xl font-bold">Merge Files</h3>
-                <p className="my-5 text-gray-500">
-                  Semper a augue suscipit, luctus neque purus ipsum neque dolor
-                  primis libero tempus velna culpa expedita.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-row gap-5">
-              <span>
-                <BsAlarm className="text-5xl text-secondary" />
-              </span>
-              <div>
-                <h3 className="text-2xl font-bold">Action Reminder</h3>
-                <p className="my-5 text-gray-500">
-                  Semper a augue suscipit, luctus neque purus ipsum neque dolor
-                  primis libero tempus velna culpa expedita.
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
